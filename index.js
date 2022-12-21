08bf9a0c4287ef4741b0e3cb86c210424244778f
Yuta Okuzono <spadeloves@gmail.com>
2022-12-21 18:11:47 +0900

    getting emoji!

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
+const dayjs = require('dayjs');
+const _ = require('lodash');
+
 // 必要ライブラリの読み込み
 const SlackBot = require('slackbots');
 const axios = require('axios');
 const dotenv = require('dotenv');
 dotenv.config();
-
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 // SLACK_CHANELは、Slack上に存在するチャンネル(例: general)を指定
@@ -26,6 +18,12 @@ const bot = new SlackBot({
     token: `${process.env.BOT_TOKEN}`,  // .envファイルのトークンを参照
     name: APP_NAME
 });
+
+let latestEmojis = []
+let addedEmojis = []
+let lastUpdatedAt = dayjs().format('YYYY-MM-DD')
+
+getLatestEmojis(true)
 
 // Slackでのメッセージ投稿の際の処理
 bot.on('message', (data) => {
@@ -41,9 +39,13 @@ bot.on('error', (err) => {
 })
 
 // メッセージごとの投稿処理
-function sendMessage(data) {
+async function sendMessage(data) {
     if(data.text.includes(' whatsnew')) {
-        getLatestEmojis(data)
+      const now = dayjs().format('YYYY-MM-DD')
+      if(now != lastUpdatedAt) {
+        await getLatestEmojis(data)
+      }
+      reportEmojisCreatedYesterday(data)
     } else if(data.text.includes(' help')) {
         getHelp()
     }
@@ -58,15 +60,54 @@ function getHelp() {
     );
 }
 
-function getLatestEmojis(data) {
-  console.log(data)
+// ============
+// Emoji
+// ============
+
+function getLatestEmojis(isFirst) {
   const emojis = bot.getEmojis()
   emojis.then( data => {
-    bot.postMessageToChannel(
-      // data.channel,
-      SLACK_CHANEL,
-      `【コマンド一覧】\nwhatsnew: 昨日に追加された絵文字を確認します\nhelp : ヘルプ情報を表示します`,
-      PARAMS
-    )
+    const emojiArray = getEmojiNameArray(data.emoji)
+    addedEmojis = _.differenceWith(emojiArray, latestEmojis)
+    latestEmojis = emojiArray
+    lastUpdatedAt = dayjs().format('YYYY-MM-DD')
+    if(isFirst) {
+      addedEmojis = latestEmojis
+    }
+    console.log('get and saved latest emoji')
+    return true
   })
 }
+
+function reportEmojisCreatedYesterday(data) {
+  let message = '昨日追加された絵文字は以下です！\n\n'
+
+  addedEmojis.forEach(emoji => {
+    message += `:${emoji}: `
+  })
+
+  console.log(message)
+
+  bot.postMessageToChannel(
+    SLACK_CHANEL,
+    message,
+    PARAMS
+  );
+}
+
+function getEmojiNameArray(emojiObject) {
+  return Object.keys(emojiObject)
+}
+
+// =-=-=-=-=-=-=-=-=
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
