f8bad3deae766599661391f06e3eaf7916dde9bb
Yuta Okuzono <spadeloves@gmail.com>
2022-12-21 21:35:58 +0900

    reply to channel, all

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ dotenv.config();
 
 
 // SLACK_CHANELは、Slack上に存在するチャンネル(例: general)を指定
-const APP_NAME = 'emoji_admin';
-const SLACK_CHANEL = 'okuzono-lab-pub';
-const PARAMS = {icon_emoji: ':robot_face:'}
+const APP_NAME = 'つまらぬ絵文字を作ってしまう五エ門';
+const PARAMS = {icon_emoji: ':goemon:'}
 
 // botインスタンスの作成
 const bot = new SlackBot({
@@ -27,7 +26,7 @@ getLatestEmojis(true)
 
 // Slackでのメッセージ投稿の際の処理
 bot.on('message', (data) => {
-    if(data.type !== 'message') {
+    if(data.type !== 'desktop_notification') {
         return;
     }
     sendMessage(data);
@@ -40,21 +39,23 @@ bot.on('error', (err) => {
 
 // メッセージごとの投稿処理
 async function sendMessage(data) {
-    if(data.text.includes(' whatsnew')) {
+    if(data.content.includes(' whatsnew')) {
       const now = dayjs().format('YYYY-MM-DD')
       if(now != lastUpdatedAt) {
-        await getLatestEmojis(data)
+        await getLatestEmojis(false)
       }
       reportEmojisCreatedYesterday(data)
-    } else if(data.text.includes(' help')) {
-        getHelp()
+    } else if(data.content.includes(' all')) {
+      reportAllEmoji(data)
+    } else if(data.content.includes(' help')) {
+      getHelp(data)
     }
 }
 
 // help情報投稿処理
 function getHelp() {
     bot.postMessageToChannel(
-        SLACK_CHANEL,
+        getChannelNameToReply(data)
         `【コマンド一覧】\nwhatsnew: 昨日に追加された絵文字を確認します\nhelp : ヘルプ情報を表示します`,
         PARAMS
     );
@@ -79,17 +80,36 @@ function getLatestEmojis(isFirst) {
   })
 }
 
-function reportEmojisCreatedYesterday(data) {
-  let message = '昨日追加された絵文字は以下です！\n\n'
+async function reportEmojisCreatedYesterday(data) {
+  let message = ''
 
-  addedEmojis.forEach(emoji => {
+  if(addedEmojis.length) {
+    message = 'また昨日にこんなつまらぬ絵文字を作ってしまった…\n\n'
+
+    addedEmojis.forEach(emoji => {
+      message += `:${emoji}: `
+    })
+  } else {
+    message = '昨日は絵文字を作っていないでござる'
+  }
+  const channel = getChannelNameToReply(data)
+  bot.postMessageToChannel(
+    channel,
+    message,
+    PARAMS
+  );
+}
+
+async function reportAllEmoji(data) {
+  const emojiData = await bot.getEmojis()
+  let message = '今までに追加された絵文字は以下でござる\n\n'
+  const emojiArray = getEmojiNameArray(emojiData.emoji)
+  emojiArray.forEach(emoji => {
     message += `:${emoji}: `
   })
-
-  console.log(message)
-
+  const channel = getChannelNameToReply(data)
   bot.postMessageToChannel(
-    SLACK_CHANEL,
+    channel,
     message,
     PARAMS
   );
@@ -99,15 +119,29 @@ function getEmojiNameArray(emojiObject) {
   return Object.keys(emojiObject)
 }
 
+// ============
+// Channel
+// ============
+
+async function getChannelNameFromId(id) {
+  const channelData = await bot.getChannels()
+  // console.log(channelData)
+  return channelData.channels.find(ch => ch.id = id)
+}
+
+function getChannelNameToReply(data) {
+  return data.subtitle.replace('#', '')
+}
+
 // =-=-=-=-=-=-=-=-=
 
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
